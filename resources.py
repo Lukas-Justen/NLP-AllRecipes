a3e30d76afc29cb1d54f37d35032d8611d06e938
Eric <eyang9001@gmail.com>
2019-02-26 12:36:35 -0600

    Updating Resources

--- resources.py
+++ resources.py
@@ -1,1 +1,10 @@
-MEATS = ['beef', 'steak', 'pork', 'chicken', '']
+MEATS = ['beef', 'steak', 'pork', 'chicken', 'goat', 'ham', 'lamb', 'turkey', 'rabbit', 'venison',
+         'elk', 'duck', 'goose', 'buffalo', 'veal']
+PROTEINS = ['beef', 'steak', 'pork', 'chicken', 'goat', 'ham', 'lamb', 'turkey', 'rabbit', 'venison',
+            'elk', 'duck', 'goose', 'buffalo', 'veal', 'tofu', 'seitan']
+VEGETARIAN_PROTEINS = ['tofu', 'seitan', 'eggplant']
+FRUITS = ['apple', 'apricot', 'banana', 'blueberry', 'cherry', 'date', 'elderberry', 'fig', 'grape',
+          'grapefruit', 'guava', 'plum', 'kiwi', 'kumquat', 'lemon', 'lime', 'lychee', 'mandarin',
+          'mango', 'nectarine', 'orange', 'papaya', 'passion fruit', 'peach', 'pear', 'persimmon',
+          'pineapple', 'plantain', 'plum', 'pomegranate', 'prune', 'pummelo', 'raspberry', 'strawberry',
+          'tangerine', 'watermelon']
